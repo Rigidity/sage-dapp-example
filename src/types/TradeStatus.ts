7b2d3d9bfc8b272573fda3c0aaac6ddcd830c367
Rigidity <me@rigidnetwork.com>
2024-12-01 09:23:19 -0500

    Initial CHIP-0002 spec

--- src/types/TradeStatus.ts
+++
@@ -1,8 +0,0 @@
-export enum TradeStatus {
-    PendingAccept = 'PENDING_ACCEPT',
-    PendingConfirm = 'PENDING_CONFIRM',
-    PendingCancel = 'PENDING_CANCEL',
-    Cancelled = 'CANCELLED',
-    Confimed = 'CONFIRMED',
-    Failed = 'FAILED',
-}
--- src/types/rpc/GetWalletBalance.ts
+++
@@ -1,7 +0,0 @@
-import { WalletBalance } from '../WalletBalance';
-
-export interface GetWalletBalanceRequest {
-    walletId?: number;
-}
-
-export type GetWalletBalanceResponse = WalletBalance;
